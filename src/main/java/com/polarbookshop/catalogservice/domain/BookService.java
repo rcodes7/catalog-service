88c981733348365038fac4ed38aec9829afc93f9
raul <rrdev.9@gmail.com>
2023-02-16 22:35:58 -0600

    define Book repository and Service

---
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
@@ -0,0 +1,49 @@
+package com.polarbookshop.catalogservice.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Iterable<Book> viewBookList() {
+        return this.bookRepository.findAll();
+    }
+
+    public Book viewBookDetails(String isbn) {
+        return this.bookRepository
+                .findByIsbn(isbn)
+                .orElseThrow(() -> new RuntimeException(isbn));
+    }
+
+    public Book addBookToCatalog(Book book) {
+        if (bookRepository.existsByIsbn(book.isbn())) {
+            throw new RuntimeException(book.isbn());
+        }
+        return bookRepository.save(book);
+    }
+
+    public void removeBookFromCatalog(String isbn) {
+        bookRepository.deleteByIsbn(isbn);
+    }
+
+    public Book editBookDetails(String isbn, Book book) {
+        return bookRepository.findByIsbn(isbn)
+                .map(existingBook -> {
+                    var bookToUpdate = new Book(
+                            existingBook.isbn(),
+                            book.title(),
+                            book.author(),
+                            book.price()
+                    );
+                    return bookRepository.save(bookToUpdate);
+                })
+                .orElseGet(() -> addBookToCatalog(book));
+    }
+
+
+}
