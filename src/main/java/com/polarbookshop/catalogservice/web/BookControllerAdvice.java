65d470ae6e2163e22d7cb72bcd756c537e17d7ae
raul <rrdev.9@gmail.com>
2023-02-18 10:30:20 -0600

    add book controller advice for book not found and book already exists

---
+++ src/main/java/com/polarbookshop/catalogservice/web/BookControllerAdvice.java
@@ -0,0 +1,27 @@
+package com.polarbookshop.catalogservice.web;
+
+import com.polarbookshop.catalogservice.domain.BookAlreadyExistsException;
+import com.polarbookshop.catalogservice.domain.BookNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class BookControllerAdvice {
+    @ExceptionHandler(BookNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String bookNotFoundHandler(BookNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(BookAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public String bookAlreadyExistsException(BookAlreadyExistsException ex) {
+        return ex.getMessage();
+    }
+
+
+
+
+}
