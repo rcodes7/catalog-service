724b523291c6ef84a2c4ee7c36010d8cbf0080c9
raul <rrdev.9@gmail.com>
2023-02-18 10:36:27 -0600

    add book controller advice for validation errors

--- src/main/java/com/polarbookshop/catalogservice/web/BookControllerAdvice.java
+++ src/main/java/com/polarbookshop/catalogservice/web/BookControllerAdvice.java
@@ -3,9 +3,14 @@ package com.polarbookshop.catalogservice.web;
 import com.polarbookshop.catalogservice.domain.BookAlreadyExistsException;
 import com.polarbookshop.catalogservice.domain.BookNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class BookControllerAdvice {
@@ -19,6 +24,14 @@ public class BookControllerAdvice {
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     public String bookAlreadyExistsException(BookAlreadyExistsException ex) {
         return ex.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        var errors = new HashMap<String, String>();
+        ex.getBindingResult().getAllErrors().forEach(error -> errors.put(((FieldError) error).getField(), error.getDefaultMessage()));
+        return errors;
     }
 
 
